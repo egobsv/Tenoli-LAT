9b75514b09e980986fce8b02d46b99fa7ee48c0e
Gobierno Electrónico <eramirez@presidencia.gob.sv>
2018-01-20 11:14:26 -0600

    edit
    edits

--- ubuntu-xenial/servidorCentral.sh
+++ ubuntu-xenial/servidorCentral.sh
@@ -1,23 +1,22 @@
 #
 #Servidor Central basado en Ubuntu-Xenial
 #
-cd /opt/ubuntu-xenial/scripts;
 timedatectl set-timezone America/El_Salvador;
 useradd -m xroad;
 echo 'xroad:xroad' | chpasswd;
+cp /vagrant/dependencias/* /var/cache/apt/archives/;
 apt-get update;
-apt-get install -y openjdk-8-jre-headless ca-certificates-java crudini rlwrap ntp jetty9 unzip;
+apt-get install -y openjdk-8-jre-headless ca-certificates-java crudini rlwrap ntp authbind unzip;
 apt-get install -y nginx-light postgresql postgresql-contrib postgresql-9.5 postgresql-client-9.5 libmhash2;
+cd /vagrant/scripts;
 debconf-set-selections sc-respuestas.txt;
 
-dpkg -i debs/xroad-common_6.16.0-1._amd64.deb debs/xroad-jetty9_6.16.0-1._all.deb;
+dpkg -i debs/xroad-common_6.16.0-1_amd64.deb debs/xroad-jetty9_6.16.0-1_all.deb;
 cp jetty.conf /etc/xroad/services/;
 cp xroad-jetty.service /etc/systemd/system/;
 cp xroad-jetty9 /usr/bin/; 
 chmod +x /usr/bin/xroad-jetty9;
 systemctl enable xroad-jetty.service;
-systemctl stop jetty9.service;
-systemctl disable jetty9.service;/ubuntu-xenial/
 systemctl start xroad-jetty.service;
 
 cp xroad-signer /usr/share/xroad/bin;
@@ -26,12 +25,13 @@ cp xroad-signer.service /etc/systemd/system/;
 systemctl enable xroad-signer.service;
 systemctl start xroad-signer.service;
 
-dpkg -i debs/xroad-center-XENIAL-6.16.deb;
+#dpkg -i debs/xroad-center-XENIAL-6.16.deb;
+dpkg -i debs/xroad-center_6.16.0-1_all.deb;
 
 cp xroad-confclient /usr/share/xroad/bin;
 chmod +x /usr/share/xroad/bin/xroad-confclient;
 cp xroad-confclient.service /etc/systemd/system/;
 systemctl enable xroad-confclient.service;
 
-dpkg -i debs/xroad-centralserver-monitoring_6.16.0-1._all.deb debs/xroad-centralserver_6.16.0-1._all.deb;
+dpkg -i debs/xroad-centralserver-monitoring_6.16.0-1_all.deb debs/xroad-centralserver_6.16.0-1_all.deb;
 systemctl start xroad-confclient.service;
--- ubuntu-xenial/servidorSeguridad.sh
+++ ubuntu-xenial/servidorSeguridad.sh
@@ -1,23 +1,20 @@
-#
 #Servidor de Seguridad basado en Ubuntu-Xenial
 #
-cd /opt/ubuntu-xenial/scripts;
 timedatectl set-timezone America/El_Salvador;
 useradd -m xroad;
 echo 'xroad:xroad' | chpasswd;
 cp /vagrant/dependencias/* /var/cache/apt/archives/;
 apt-get update;
-apt-get install -y openjdk-8-jre-headless ca-certificates-java crudini rlwrap ntp jetty9 unzip expect;
+apt-get install -y openjdk-8-jre-headless ca-certificates-java crudini rlwrap ntp unzip expect authbind;
 apt-get install -y nginx-light postgresql postgresql-contrib postgresql-9.5 postgresql-client-9.5 libmhash2;
+cd /vagrant/scripts;
 debconf-set-selections ss-respuestas.txt;
 
-dpkg -i debs/xroad-common_6.16.0-1._amd64.deb debs/xroad-jetty9_6.16.0-1._all.deb;
+dpkg -i debs/xroad-common_6.16.0-1_amd64.deb debs/xroad-jetty9_6.16.0-1_all.deb;
 cp jetty.conf /etc/xroad/services/;
 cp xroad-jetty.service /etc/systemd/system/;
 cp xroad-jetty9 /usr/bin/; 
 chmod +x /usr/bin/xroad-jetty9;
-systemctl stop jetty9.service;
-systemctl disable jetty9.service;
 systemctl enable xroad-jetty.service;
 systemctl start xroad-jetty.service;
 
@@ -48,13 +45,19 @@ cp xroad-autologin.service /etc/systemd/system/;
 systemctl enable xroad-proxy.service;
 systemctl enable xroad-monitor.service;
 systemctl enable xroad-opmonitor.service;
-systemctl enable xroad-autologin.service;
 
-dpkg -i --force-confnew debs/xroad-proxy_6.16.0-1._all.deb debs/xroad-addon-wsdlvalidator_6.16.0-1._all.deb;
-dpkg -i debs/xroad-monitor_6.16.0-1._all.deb  debs/xroad-opmonitor_6.16.0-1._all.deb debs/xroad-addon-proxymonitor_6.16.0-1._all.deb; 
-dpkg -i debs/xroad-addon-messagelog_6.16.0-1._all.deb debs/xroad-addon-metaservices_6.16.0-1._all.deb;
-dpkg -i debs/xroad-securityserver_6.16.0-1._all.deb debs/xroad-autologin_6.16.0-1._all.deb;
+dpkg -i --force-confnew debs/xroad-proxy_6.16.0-1_all.deb debs/xroad-addon-wsdlvalidator_6.16.0-1_all.deb;
+dpkg -i debs/xroad-monitor_6.16.0-1_all.deb  debs/xroad-opmonitor_6.16.0-1_all.deb debs/xroad-addon-proxymonitor_6.16.0-1_all.deb; 
+dpkg -i debs/xroad-addon-messagelog_6.16.0-1_all.deb debs/xroad-addon-metaservices_6.16.0-1_all.deb;
+dpkg -i debs/xroad-securityserver_6.16.0-1_all.deb debs/xroad-autologin_6.16.0-1_all.deb;
+
+touch /etc/xroad/autologin;
+chown xroad:xroad /etc/xroad/autologin;
 systemctl start xroad-proxy.service;
 systemctl start xroad-monitor.service;
 systemctl start xroad-opmonitor.service;
+
+##NUMERO PIN 
+echo "12345" >> /etc/xroad/autologin;
+systemctl enable xroad-autologin.service;
 systemctl start xroad-autologin.service;
