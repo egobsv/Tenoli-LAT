666ec2931eed03655f523bd4d1e993fbbbf57c5e
Gobierno Electrónico <31377405+egobsv@users.noreply.github.com>
2018-04-14 17:55:38 -0600

    Update ocsp.sh

--- ubuntu-xenial/openssl-ca/home/ca/CA/ocsp.sh
+++ ubuntu-xenial/openssl-ca/home/ca/CA/ocsp.sh
@@ -1,1 +1,1 @@
-openssl ocsp -index index.txt -port 8888 -rsigner certs/ocsp.cert.pem -rkey private/ocsp.key.pem -CA certs/ca.cert.pem -text -out log.txt
+openssl ocsp -index index.txt -port 8888 -rsigner certs/ocsp.cert.pem -rkey private/ocsp.key.pem -CA certs/ca.cert.pem -text -out /var/log/ocsp.log
