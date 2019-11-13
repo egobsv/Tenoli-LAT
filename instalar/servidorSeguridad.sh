fbf0f9f227a160f3e4efb4d31e360b0a63ae2ca0
Gobierno Electrónico <31377405+egobsv@users.noreply.github.com>
2019-11-13 11:55:33 -0600

    Update servidorSeguridad.sh

--- instalar/servidorSeguridad.sh
+++ instalar/servidorSeguridad.sh
@@ -17,22 +17,22 @@ apt-get install -y openjdk-8-jre-headless ca-certificates-java ntp unzip expect 
 
 debconf-set-selections /vagrant/ss-respuestas.txt;
 cd /opt/tenoli;
-
-dpkg -i xroad-base_6.22.0-1.22.0.ubuntu18.04_amd64.deb xroad-jetty9_6.22.0-1.22.0.ubuntu18.04_all.deb \
-	 xroad-signer_6.22.0-1.22.0.ubuntu18.04_amd64.deb xroad-nginx_6.22.0-1.22.0.ubuntu18.04_amd64.deb \
-         xroad-confclient_6.22.0-1.22.0.ubuntu18.04_amd64.deb;
+                   
+dpkg -i xroad-base_6.22.0-1.local.ubuntu18.04_amd64.deb xroad-jetty9_6.22.0-1.22.0.ubuntu18.04_all.deb \
+	 xroad-signer_6.22.0-1.local.ubuntu18.04_amd64.deb xroad-nginx_6.22.0-1.local.ubuntu18.04_amd64.deb \
+         xroad-confclient_6.22.0-1.local.ubuntu18.04_amd64.deb;
 service postgresql restart;
 
-dpkg -i xroad-proxy_6.22.0-1.22.0.ubuntu18.04_all.deb xroad-monitor_6.22.0-1.22.0.ubuntu18.04_all.deb \
-        xroad-opmonitor_6.22.0-1.22.0.ubuntu18.04_all.deb \
-        xroad-addon-opmonitoring_6.22.0-1.22.0.ubuntu18.04_all.deb; 
+dpkg -i xroad-proxy_6.22.0-1.local.ubuntu18.04_all.deb  xroad-monitor_6.22.0-1.local.ubuntu18.04_all.deb  \
+        xroad-opmonitor_6.22.0-1.local.ubuntu18.04_all.deb  \
+        xroad-addon-opmonitoring_6.22.0-1.local.ubuntu18.04_all.deb; 
 
-dpkg -i xroad-addon-metaservices_6.22.0-1.22.0.ubuntu18.04_all.deb \
-		xroad-addon-messagelog_6.22.0-1.22.0.ubuntu18.04_all.deb \
-		xroad-addon-proxymonitor_6.22.0-1.22.0.ubuntu18.04_all.deb \
-		xroad-addon-wsdlvalidator_6.22.0-1.22.0.ubuntu18.04_all.deb;
+dpkg -i xroad-addon-metaservices_6.22.0-1.local.ubuntu18.04_all.deb \
+		xroad-addon-messagelog_6.22.0-1.local.ubuntu18.04_all.deb  \
+		xroad-addon-proxymonitor_6.22.0-1.local.ubuntu18.04_all.deb  \
+		xroad-addon-wsdlvalidator_6.22.0-1.local.ubuntu18.04_all.deb ;
 
-dpkg -i xroad-securityserver_6.22.0-1.22.0.ubuntu18.04_all.deb xroad-autologin_6.22.0-1.22.0.ubuntu18.04_all.deb;
+dpkg -i xroad-securityserver_6.22.0-1.local.ubuntu18.04_all.deb  xroad-autologin_6.22.0-1.local.ubuntu18.04_all.deb;
 ##NUMERO PIN
 touch /etc/xroad/autologin;
 chown xroad:xroad /etc/xroad/autologin;
